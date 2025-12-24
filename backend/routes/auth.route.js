0aabe84280103749d797e771c55bff344f023a0a
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-25 00:24:53 +0530

    image upload

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signUp,signIn,userProfile,updateUserProfile } from "../controllers/auth.controller.js";
+import { signUp,signIn,userProfile,updateUserProfile,uploadImage } from "../controllers/auth.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.post("/sign-up",signUp);
 router.post("/sign-in",signIn);
 router.get("/user-profile",verifyToken,userProfile);
 router.put("/update-profile",verifyToken,updateUserProfile);
+router.post("/upload-image",upload.single("image"),uploadImage);
 
 export default router;
