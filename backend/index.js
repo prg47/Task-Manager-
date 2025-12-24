3d3a34466764ba33737b103f6d5ee649be207551
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-24 20:13:11 +0530

    middleware - cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+//middleware for cors
+app.use(cors({
+    origin: process.env.FRONT_END_URL || "http://localhost:5173/",
+    methods: ["GET","PUT","POST","DELETE"],
+    allowedHeaders: ["Content-Type","Authorization"],
+}))
+
+
+// middleware for json
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("server running");
