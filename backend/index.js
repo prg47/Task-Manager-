344b99b9587194091e5d972dd0433fadf3bb8a93
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-24 20:35:31 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database connected");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
