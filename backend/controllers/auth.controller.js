336377b20c0972b16bdae78a026d716e07af8852
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-24 23:37:10 +0530

    get user profile api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -71,3 +71,18 @@ export const signIn = async (req,res,next)=>{
         next(error)
     }
 }
+
+export const userProfile = async(req,res,next) => {
+    try{
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+            return next(errorHandler(404,"User not found"));
+        }
+
+        const {password: pass, ...rest} = user._doc;
+        res.status(200).json(rest);
+    }catch(error){
+        next(error)
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signUp,signIn } from "../controllers/auth.controller.js";
+import { signUp,signIn,userProfile } from "../controllers/auth.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/sign-up",signUp);
 router.post("/sign-in",signIn);
+router.get("/user-profile",verifyToken,userProfile);
 
 export default router;
