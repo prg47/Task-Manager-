683cac53c7d1acd7260212b910edfce5ddea3b63
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-24 23:52:11 +0530

    update-profile api route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signUp,signIn,userProfile } from "../controllers/auth.controller.js";
+import { signUp,signIn,userProfile,updateUserProfile } from "../controllers/auth.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/sign-up",signUp);
 router.post("/sign-in",signIn);
 router.get("/user-profile",verifyToken,userProfile);
+router.put("/update-profile",verifyToken,updateUserProfile);
 
 export default router;
