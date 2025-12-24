ca9b0a0523e950e3d1c7d3777c02768705be4e57
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-25 01:21:51 +0530

    get user by id api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsers,getUserById } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 //user management route
-router.get("/get-users",verifyToken,adminOnly,getUsers)
+router.get("/get-users",verifyToken,adminOnly,getUsers);
+
+router.get("/:id", verifyToken,getUserById);
 
 export default router;
