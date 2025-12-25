aed2cc8eab737e6470bbc480edfbab4463b1ad08
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-25 17:12:35 +0530

    get dashboard api route(admin)

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -209,3 +209,72 @@ export const updateTaskChecklist = async(req,res,next)=>{
         next(error);
     }
 }
+
+export const getDashboardData = async(req,res,next)=>{
+    try{
+        //fetch stats
+        const totalTasks = await Task.countDocuments();
+        const pendingTasks = await Task.countDocuments({status: "Pending"});
+        const CompletedTasks = await Task.countDocuments({status: "Completed"});
+        const overdueTasks = await Task.countDocuments({
+            status: {$ne: "Completed"},
+            dueDate: {$lt: new Date()}
+        })
+
+        const taskStatuses = ["Pending","In Progress","Completed"]
+
+        const taskDistributionRaw = await Task.aggregate([
+            {
+                $group:{
+                    _id: "$status",
+                    count: {$sum: 1}
+                },
+            },
+        ])
+
+        const taskDistribution = taskStatuses.reduce((acc,status)=>{
+            const formattedKey = status.replace(/\s+/g,"");//remove spaces
+            acc[formattedKey] = taskDistributionRaw.find((item)=>item._id===status)?.count || 0
+
+            return acc
+        },{})
+
+        taskDistribution["All"] = totalTasks;
+
+        const taskPriorities = ["Low","Medium","High"]
+
+        const taskPriorityRaw = await Task.aggregate([
+            {
+                $group:{
+                    _id: "$priority",
+                    count: {$sum:1},
+                },
+
+        },])
+
+        const taskPriorityLevel = taskPriorities.reduce((acc,priority)=>{
+            acc[priority] = taskPriorityRaw.find((item)=>item._id===priority)?. count || 0
+            return acc
+        },{})
+
+        //Fetch recent 10 tasks
+        const recentTasks = await Task.find().sort({createdAt: -1}).limit(10).select("title status priority dueDate createdAt");
+
+        res.status(200).json({
+            statistics:{
+                totalTasks,
+                pendingTasks,
+                CompletedTasks,
+                overdueTasks,
+            },
+            charts: {
+                taskDistribution,
+                taskPriorityLevel,
+            },
+            recentTasks,
+        })
+
+    }catch(error){
+        next(error);
+    }
+}
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,15 +1,19 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { createTask,getTasks,getTaskById,updateTask,deleteTask,updateTaskStatus,updateTaskChecklist } from "../controllers/task.controller.js";
+import { createTask,getTasks,getTaskById,updateTask,deleteTask,updateTaskStatus,updateTaskChecklist,getDashboardData } from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken,adminOnly,createTask);
 router.get("/",verifyToken,getTasks);
+router.get("/dashboard-data",verifyToken,adminOnly,getDashboardData);
+
+
+
 router.get("/:id",getTaskById);
 router.put("/:id",updateTask);
 router.delete("/:id",verifyToken,adminOnly,deleteTask);
 router.put("/:id/status",verifyToken,updateTaskStatus);
-router.put("/:id/todo",verifyToken,updateTaskChecklist)
+router.put("/:id/todo",verifyToken,updateTaskChecklist);
 
 export default router;
