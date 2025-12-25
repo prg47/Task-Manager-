0d4bca191c7bafea895df37e26638c6df9382519
prg47 <115251712+prg47@users.noreply.github.com>
2025-12-25 10:46:28 +0530

    get task by id

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { createTask,getTasks } from "../controllers/task.controller.js";
+import { createTask,getTasks,getTaskById } from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken,adminOnly,createTask);
 router.get("/",verifyToken,getTasks);
+router.get("/:id",getTaskById);
 
 export default router;
